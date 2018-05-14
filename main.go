681a9ccc3bbc17bfd37121d0295119d4fa6aaf0f
okcy1016 <okcy1016@gmail.com>
2018-05-14 22:06:27 +0800

    v1.2 GUI uploaded

--- main.go
+++ main.go
@@ -1,9 +1,52 @@
 package main
-	
+
+import (
+	"github.com/zserge/webview"
+)
+
+const (
+	windowWidth  = 800
+	windowHeight = 600
+)
+
+
+func HandleRPC(w webview.WebView, convCfgStr string) {
+
+	saveCfgToDisk(convCfgStr)
+
+	// Passing global cmd (*exec.Cmd) argument
+	cmdGlo = restartCliApp(cmdGlo, convCfgStr)
+}
 
 func main() {
 
-	StartCliApp()
+	ifDae := false
+	convCfgStr := loadCfgFromDisk()
+
+	// Can't use ":=" here
+	cmdGlo, ifDae = startCliApp(convCfgStr)
+	
+	url := StartHTTPServ()
+	
+	w := webview.New(webview.Settings{
+		Width:     windowWidth,
+		Height:    windowHeight,
+		Title:     "Poetry desktop",
+		Resizable: true,
+		URL:       url,
+		ExternalInvokeCallback: HandleRPC,
+	})
+
+
+	defer w.Exit()
+	w.Run()
+
+	// If daemon
+	if ifDae == false {
+		
+		// Use this no argument function is because
+		// what passed to function in a defer scope won't be changed anymore
+		// but when we restart the cli app, the cmdGlo will be changed
+		defer StopCliAppWithoutArgv()
+	}
 }
-	
-	
